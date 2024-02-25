7347606f391cb8bdf6f3cd9de587240473f2333b
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-02-25 00:59:11 -0500

    changed text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h3>Welcome to the basic GPT wrapper</h3>
+<p>this might eventually become something if I can be bothered...</p>
