90269f445a092bff8790d08c11b1def904aa3b17
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-02-27 17:23:39 -0500

    new db table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -4,3 +4,8 @@ export const todos = pgTable('todos', {
 	id: serial('id').primaryKey(),
 	name: varchar('name', { length: 255 }).notNull()
 });
+
+export const demo = pgTable('demo', {
+	id: serial('id').primaryKey(),
+	value: varchar('value', { length: 255 }).notNull()
+});
